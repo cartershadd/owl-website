84f72ef2c1d509563484ec568ee812b5adfce7ca
Carter Shadden <carter.shadden@gmail.com>
2019-08-07 16:47:31 -0400

    condensed components to one page, fixed search result issues

--- src/App.js
+++ src/App.js
@@ -3,15 +3,9 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import NavBar from "./layout/navBar";
 import About from "./layout/About";
-import OwlPreview from "./components/owls/OwlPreview";
-import OwlProfile from "./components/owls/OwlProfile";
-import SalamanderPreview from './components/salamanders/SalamanderPreview';
-import SalamanderProfile from './components/salamanders/SalamanderProfile';
 import Search from "./layout/Search";
-import FrogProfile from "./components/frogs/FrogProfile";
-import FrogPreview from "./components/frogs/FrogPreview";
-import FoxPreview from "./components/foxes/FoxPreview";
-import FoxProfile from "./components/foxes/FoxProfile";
+import AnimalPreview from "./components/animals/AnimalPreview";
+import AnimalProfile from './components/animals/AnimalProfile';
 
 function App() {
     return (
@@ -22,14 +16,14 @@ function App() {
                     <Switch>
                         <Route exact path='/home' component={Search}/>
                         <Route exact path='/about' component={About}/>
-                        <Route exact path='/owl' component={OwlPreview}/>
-                        <Route exact path='/owl/:id' component={OwlProfile}/>
-                        <Route exact path='/fox' component={FoxPreview}/>
-                        <Route exact path='/fox/:id' component={FoxProfile}/>
-                        <Route exact path='/salamander' component={SalamanderPreview}/>
-                        <Route exact path='/salamander/:id' component={SalamanderProfile}/>
-                        <Route exact path='/frog' component={FrogPreview}/>
-                        <Route exact path='/frog/:id' component={FrogProfile}/>
+                        <Route exact path='/:type' component={AnimalPreview}/>
+                        <Route exact path='/:type/:id' component={AnimalProfile}/>
+                        <Route exact path='/:type' component={AnimalPreview}/>
+                        <Route exact path='/:type/:id' component={AnimalProfile}/>
+                        <Route exact path='/:type' component={AnimalPreview}/>
+                        <Route exact path='/:type/:id' component={AnimalProfile}/>
+                        <Route exact path='/:type' component={AnimalPreview}/>
+                        <Route exact path='/:type/:id' component={AnimalProfile}/>
                     </Switch>
                 </div>
 
--- src/layout/Search.js
+++ src/layout/Search.js
@@ -17,6 +17,7 @@ class Search extends Component {
                 ],
                 about: 'Barn owls are sometimes referred to as the ghost owl.',
                 facts: 'They have a cry that sounds like a horrifying shriek. As the name suggests, they do indeed love to roost in barns and abandoned buildings.',
+                type: 'owl'
             },
             {
                 id: 2,
@@ -30,6 +31,7 @@ class Search extends Component {
                 ],
                 about: 'Barred owls do not migrate.',
                 facts: 'This owl has a call that sounds like the phrase, "Who cooks for you?" This often sounds like the barking of a large dog from a distance.',
+                type: 'owl'
             },
             {
                 id: 3,
@@ -43,6 +45,7 @@ class Search extends Component {
                 ],
                 about: 'Snowy owls are amazing and beautiful!',
                 facts: 'They live in the colder regions of North America. Hedwig from the Harry Potter series is a snowy owl.',
+                type: 'owl'
             },
             {
                 id: 4,
@@ -56,6 +59,7 @@ class Search extends Component {
                 ],
                 about: 'The eastern screech owl looks like a mini version of the great horned owl.',
                 facts: 'The eastern screech owl can be grey or red. They are approximately the same size as a robin.',
+                type: 'owl'
             },
             {
                 id: 5,
@@ -69,7 +73,8 @@ class Search extends Component {
                 ],
                 about: 'The tiger salamander has stripes and an adorable smile. They can live up to 25 years old!',
                 facts: 'Salamanders are amphibians and undergo metamorphosis like frogs and toads do,' +
-                    'although there are some exceptions.'
+                    'although there are some exceptions.',
+                type: 'salamander'
             },
             {
                 id: 6,
@@ -83,7 +88,8 @@ class Search extends Component {
                 ],
                 about: 'The marbled salamander has beautiful black and white markings that vary from ' +
                     'salamander to salamander.',
-                facts: 'The female marbled salamander will guard her eggs until they hatch.'
+                facts: 'The female marbled salamander will guard her eggs until they hatch.',
+                type: 'salamander'
             },
             {
                 id: 7,
@@ -97,7 +103,8 @@ class Search extends Component {
                 ],
                 about: 'There are several genetically distinct species of Slimy Salamander' +
                     'in Georgia alone, many of which have subtle physical differences (or none at all).',
-                facts: 'Slimy salamanders lay eggs on land, and once the larvae metamorphose, they resemble the adults.'
+                facts: 'Slimy salamanders lay eggs on land, and once the larvae metamorphose, they resemble the adults.',
+                type: 'salamander'
             },
             {
                 id: 8,
@@ -111,7 +118,8 @@ class Search extends Component {
                 ],
                 about: 'The red salamander is found throughout the eastern United States, and thankfully they are quite common and so far,' +
                     'their populations remain intact.',
-                facts: 'It takes 2 to 3 years for red salamander larvae to metamorphose.'
+                facts: 'It takes 2 to 3 years for red salamander larvae to metamorphose.',
+                type: 'salamander'
             },
             {
                 id: 9,
@@ -126,7 +134,8 @@ class Search extends Component {
                 about: 'The American bullfrog is found often in both rural and urban settings. It does' +
                     'not seem uncomfortable living among humans.',
                 facts: 'Bullfrogs often share space with fish, which is unusual compared to other frogs, who compete ' +
-                    'for space with fish often.'
+                    'for space with fish often.',
+                type: 'frog'
             },
             {
                 id: 10,
@@ -140,7 +149,8 @@ class Search extends Component {
                 ],
                 about: 'Fun fact - frogs and toads are basically the same thing. There is of course only one family of true toads, but to call them frogs is not technically incorrect.',
                 facts: 'The American toad may often be mistaken for other toads - oftentimes, if you count the warts, you can spot the difference.' +
-                    'The call of the American toad sounds like a long musical trill, which is distinct from other toads.'
+                    'The call of the American toad sounds like a long musical trill, which is distinct from other toads.',
+                type: 'frog'
             },
             {
                 id: 11,
@@ -153,7 +163,8 @@ class Search extends Component {
                     process.env.PUBLIC_URL + '/images/narrow5.jpg'
                 ],
                 about: 'The Eastern Narrowmouth toad has a call similar to a bleating sheep. While not common, the female of the species can lay up to 700 eggs in a breeding event/',
-                facts: 'They also have an adorable narrow little face!'
+                facts: 'They also have an adorable narrow little face!',
+                type: 'frog'
             },
             {
                 id: 12,
@@ -166,7 +177,8 @@ class Search extends Component {
                     process.env.PUBLIC_URL + '/images/peep5.jpg'
                 ],
                 about: 'The call of the spring peeper is quite deafening when many of them congregate!',
-                facts: 'They have a distinctive cross on their back, which is where they get their scientific name Pseudacris crucifer.'
+                facts: 'They have a distinctive cross on their back, which is where they get their scientific name Pseudacris crucifer.',
+                type: 'frog'
             },
             {
                 id: 13,
@@ -180,7 +192,8 @@ class Search extends Component {
                 ],
                 about: 'The red fox is the largest of the true foxes and is found throughout North America and the entire world.',
                 facts: 'While foxes can live quite well among humans, many people want to keep them as pets and are terribly unprepared to do so.' +
-                    'The red fox is quite beautiful and harmless to humans, but must remain wild.'
+                    'The red fox is quite beautiful and harmless to humans, but must remain wild.',
+                type: 'fox'
             },
             {
                 id: 14,
@@ -194,7 +207,8 @@ class Search extends Component {
                 ],
                 about: 'The Arctic fox as two morphs - white and blue. White Arctic foxes will turn white in winter and be brown- gray in summer. A blue morph Arctic fox is blue to dark gray all year around - however, over ' +
                     '99% of the population is the white morph.',
-                facts: 'Arctic foxes are mostly monogamous and both parents care for the young.'
+                facts: 'Arctic foxes are mostly monogamous and both parents care for the young.',
+                type: 'fox'
             },
             {
                 id: 15,
@@ -207,7 +221,8 @@ class Search extends Component {
                     process.env.PUBLIC_URL + '/images/fennec5.jpg'
                 ],
                 about: 'The fennec fox is found in desert climates and its large ears help keep it from overheating.',
-                facts: 'The fennec fox is small but has very loud vocalizations. It eats mostly insects and small birds.'
+                facts: 'The fennec fox is small but has very loud vocalizations. It eats mostly insects and small birds.',
+                type: 'fox'
             },
             {
                 id: 16,
@@ -220,7 +235,8 @@ class Search extends Component {
                     process.env.PUBLIC_URL + '/images/bfox5.jpg'
                 ],
                 about: 'The bat-eared fox lives in the African Savannah. The large ears help them hunt for insects and aid in thermoregulation.',
-                facts: 'Due to its insectivorous diet, the bat-eared fox has very unusual teeth compared to other fox species.'
+                facts: 'Due to its insectivorous diet, the bat-eared fox has very unusual teeth compared to other fox species.',
+                type: 'fox'
             }
         ],
         filteredAnimalList: [],
@@ -260,7 +276,7 @@ class Search extends Component {
                 <div className="search-results">
                     {this.state.filteredAnimalList.map((animal, index) => {
                         return <div key={index} className="animal-preview">
-                            <Link to={"/animal/" + animal.id} className="profile-preview">
+                            <Link to={`/${animal.type}/${animal.id}`} className="profile-preview">
                                 <span className="profile-preview-name">{animal.name}</span>
                                 <img src={animal.img[1]} alt="animal" className="animal-img"/>
 
